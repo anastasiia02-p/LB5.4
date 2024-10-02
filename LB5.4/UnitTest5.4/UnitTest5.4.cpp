da69db2a048791c3081fe1dcb102fb8684588297
anastasiia02-p <anastasiia.prunko.ik.2024@lpnu.ua>
2024-10-02 21:59:40 +0300

    Add files via upload

---
+++ LB5.4/UnitTest5.4/UnitTest5.4.cpp
@@ -0,0 +1,48 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../LB5.4/main.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest54
+{
+	TEST_CLASS(UnitTest54)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			double c;
+			c = S0(10);
+			Assert::AreEqual(c, 15.6973, 0.01);
+		}
+
+		TEST_METHOD(TestMethod2)
+		{
+			double q;
+			q = S1(10, 10);
+			Assert::AreEqual(q, 15.6973, 0.01);
+		}
+
+		TEST_METHOD(TestMethod3)
+		{
+			double w;
+			w = S2(10, 22); 
+		    Assert::AreEqual(w, 15.6973, 0.01);
+		}
+
+		TEST_METHOD(TestMethod4)
+		{
+			double e;
+			e = S3(10, 10, 10);
+			Assert::AreEqual(e, 25.6973, 0.001);
+		}
+
+		TEST_METHOD(TestMethod5)
+		{
+			double r;
+			r = S4(10, 22, 10);
+			Assert::AreEqual(r, 25.6973, 0.001);
+		}
+	};
+}
